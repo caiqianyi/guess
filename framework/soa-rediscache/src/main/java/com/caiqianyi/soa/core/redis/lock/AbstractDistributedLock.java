692596699c9d124389a12501dedc3aee20ec0505
caiqianyi <270852221@qq.com>
2018-01-27 20:45:53 +0800

    1

---
+++ framework/soa-rediscache/src/main/java/com/caiqianyi/soa/core/redis/lock/AbstractDistributedLock.java
@@ -0,0 +1,36 @@
+package com.caiqianyi.soa.core.redis.lock;
+
+/**
+ * 抽象类，实现基本的方法，关键方法由子类去实现
+ * 如果需要使用其他的分布式锁实现，继承 AbstractDistributedLock 后实现获取锁和释放锁的方法即可
+ * @author Caiqianyi
+ *
+ */
+public abstract class AbstractDistributedLock implements DistributedLock {
+
+    @Override
+    public boolean lock(String key) {
+        return lock(key, TIMEOUT_MILLIS, RETRY_TIMES, SLEEP_MILLIS);
+    }
+
+    @Override
+    public boolean lock(String key, int retryTimes) {
+        return lock(key, TIMEOUT_MILLIS, retryTimes, SLEEP_MILLIS);
+    }
+
+    @Override
+    public boolean lock(String key, int retryTimes, long sleepMillis) {
+        return lock(key, TIMEOUT_MILLIS, retryTimes, sleepMillis);
+    }
+
+    @Override
+    public boolean lock(String key, long expire) {
+        return lock(key, expire, RETRY_TIMES, SLEEP_MILLIS);
+    }
+
+    @Override
+    public boolean lock(String key, long expire, int retryTimes) {
+        return lock(key, expire, retryTimes, SLEEP_MILLIS);
+    }
+
+}
---
+++ framework/soa-rediscache/src/main/java/com/caiqianyi/soa/core/redis/lock/config/DistributedLockAspectConfiguration.java
@@ -0,0 +1,79 @@
+package com.caiqianyi.soa.core.redis.lock.config;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import com.caiqianyi.commons.exception.I18nMessageException;
+import com.caiqianyi.soa.core.redis.lock.DistributedLock;
+import com.caiqianyi.soa.core.redis.lock.RedisLock;
+import com.caiqianyi.soa.core.redis.lock.RedisLock.LockFailAction;
+
+/**
+ * @author caiqianyi
+ * @date 2017年6月14日 下午3:11:22
+ * @version 1.0.0
+ */
+@Aspect
+@Configuration
+@ConditionalOnClass(DistributedLock.class)
+@AutoConfigureAfter(DistributedLockAutoConfiguration.class)
+public class DistributedLockAspectConfiguration {
+    
+    private final Logger logger = LoggerFactory.getLogger(DistributedLockAspectConfiguration.class);
+    
+    @Autowired
+    private DistributedLock distributedLock;
+
+    @Pointcut("@annotation(com.caiqianyi.soa.core.redis.lock.RedisLock)")
+    private void lockPoint(){
+        
+    }
+    
+    @Around("lockPoint()")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable{
+        Method method = ((MethodSignature) pjp.getSignature()).getMethod();
+        RedisLock redisLock = method.getAnnotation(RedisLock.class);
+        String key = redisLock.value();
+        if(StringUtils.isEmpty(key)){
+            Object[] args = pjp.getArgs();
+            key = Arrays.toString(args);
+        }else{
+        	Object[] args = pjp.getArgs();
+        	for(int i=0;i<args.length;i++){
+        		Object arg = args[i];
+        		if(arg != null){
+        			String v = "#arg"+i;
+        			key = key.replaceAll(v, arg.toString());
+        		}
+        	}
+        }
+        int retryTimes = redisLock.action().equals(LockFailAction.CONTINUE) ? redisLock.retryTimes() : 0;
+        boolean lock = distributedLock.lock(key, redisLock.keepMills(), retryTimes, redisLock.sleepMills());
+        if(!lock) {
+            logger.error("get lock failed : " + key);
+            throw new I18nMessageException("10016");
+        }
+        
+        //得到锁,执行方法，释放锁
+        logger.debug("get lock success : " + key);
+        try {
+            return pjp.proceed();
+        } finally {
+            boolean releaseResult = distributedLock.releaseLock(key);
+            logger.debug("release lock : " + key + (releaseResult ? " success" : " failed"));
+        }
+    }
+}
