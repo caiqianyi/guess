3d1ca833bcbccb68fdf1b6d8d479ae686135b106
caiqianyi <270852221@qq.com>
2018-02-11 10:04:20 +0800

    1

--- statics/guess-statics/src/main/webapp/lib/base.js
+++ statics/guess-statics/src/main/webapp/lib/base.js
@@ -11,53 +11,51 @@ function include(i){
 	if(i >= importFiles.length){
 		return;
 	}
-	
 	if(typeof static_version == "undefined"){
 		static_version = sys_version;
 	}
-	
 	var head = document.getElementsByTagName('head')[0];
 	var file = importFiles[i];
+	var item = null;
 	if(file.type == 'css'){
-		var link = document.createElement('link');
-	    link.href = file.src+'?v='+static_version;
-	    link.rel = 'stylesheet';
-	    link.type = 'text/css';
-	    head.appendChild(link);
-	    link.addEventListener('load', function () {
-	    	include(++i);
-        }, false);
+		item = document.createElement('link');
+		item.href = file.src+'?v='+static_version;
+		item.rel = 'stylesheet';
+	    item.type = 'text/css';
 	}
 	if(file.type == 'script'){
-		var js = document.createElement('script');
-        js.setAttribute('type', 'text/javascript'); 
-        js.setAttribute('src', file.src+'?v='+static_version);
-        js.setAttribute('charset', 'UTF-8');
-        head.appendChild(js);
-        
-        js.addEventListener('load', function () {
-        	include(++i);
-        }, false);
+		item = document.createElement('script');
+		item.setAttribute('type', 'text/javascript'); 
+		item.setAttribute('src', file.src+'?v='+static_version);
+		item.setAttribute('charset', 'UTF-8');
+	}
+	if(item){
+		head.appendChild(item);
+		item.addEventListener('load', function () {
+			include(++i);
+		}, false);
 	}
 }
 
 Date.prototype.format = function(format) {
- var o = {
- "M+" : this.getMonth()+1, //month
- "d+" : this.getDate(),    //day
- "h+" : this.getHours(),   //hour
- "m+" : this.getMinutes(), //minute
- "s+" : this.getSeconds(), //second
- "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
- "S" : this.getMilliseconds() //millisecond
- }
- if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
- (this.getFullYear()+"").substr(4 - RegExp.$1.length));
- for(var k in o)if(new RegExp("("+ k +")").test(format))
- format = format.replace(RegExp.$1,
- RegExp.$1.length==1 ? o[k] :
- ("00"+ o[k]).substr((""+ o[k]).length));
- return format;
+	var o = {
+		"M+" : this.getMonth() + 1, // month
+		"d+" : this.getDate(), // day
+		"h+" : this.getHours(), // hour
+		"m+" : this.getMinutes(), // minute
+		"s+" : this.getSeconds(), // second
+		"q+" : Math.floor((this.getMonth() + 3) / 3), // quarter
+		"S" : this.getMilliseconds()
+	// millisecond
+	}
+	if (/(y+)/.test(format))
+		format = format.replace(RegExp.$1, (this.getFullYear() + "")
+				.substr(4 - RegExp.$1.length));
+	for ( var k in o)
+		if (new RegExp("(" + k + ")").test(format))
+			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k]
+					: ("00" + o[k]).substr(("" + o[k]).length));
+	return format;
 }
 
 //获取
@@ -77,6 +75,7 @@ var importFiles = [
 	{src:"/lib/myfrozen.js",type:"script"},
 	{src:"/lib/oauth2.js",type:"script"},
 ];
+
 window.onload = function(){  
 	include(0);
 }  
